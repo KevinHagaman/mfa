6a8cc3b0864df0bcf6567ef26895be91dbf7541d
Kevin Hagaman <kevinhagaman56@gmail.com>
2019-07-10 09:44:09 -0400

    Fixed auth error

--- example/auth.py
+++ example/auth.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
+from django.contrib import auth
 '''
 def loginView(request):
     context={}
